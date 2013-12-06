cb3d09a67acaa965b73a3a5db2c4bf09da1334dd
Danny Varner <danny.varner@gmail.com>
2013-12-06 11:43:20 -0800

    adding debug flag to webserver

--- apptime/apptime_api.py
+++ apptime/apptime_api.py
@@ -57,6 +57,7 @@ def users():
 def start_server():
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+    app.debug = True
 
 if __name__ == "__main__":
     start_server()
