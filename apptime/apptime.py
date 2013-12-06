1066d25e44a677933b52787cfadfc59a477eb152
Danny Varner <danny.varner@gmail.com>
2013-12-05 17:36:18 -0800

    Adding registration route and categories

--- apptime/apptime.py
+++ apptime/apptime.py
@@ -3,18 +3,28 @@ import flask
 import json
 import logging
 import os
+import uuid
 
 app = Flask(__name__)
 logging.basicConfig(level=logging.INFO)
 
-@app.route("/apptime/devices/<id>/usage", methods=["POST", "GET"])
-def hello(id):
-    return flask.jsonify(**{ "usage": [
-                   {"Appname": "Super mario brothers"},
-                   {"Appname": "Frogger"}
-                   ]
-    })
+@app.route("/apptime/devices/<id>/usage", methods=["GET", "POST"])
+def usage(id):
+    if flask.request.method == 'GET':
+        return flask.jsonify(**{ "usage": [
+                       {"appname": "Super mario brothers", "category": "Game"},
+                       {"appname": "Frogger", "category": "Game" }
+                       ]
+        })
+    if flask.request.method == 'POST':
+        logging.info("Received %s", flask.request.data)
+        return flask.jsonify(**{})
 
+@app.route("/apptime/device", methods=["POST"])
+def device():
+    data = json.loads(json.dumps(flask.request.data))
+    #logging.info("Registering new device for %s", data["name"])
+    return flask.jsonify(**{"id":str(uuid.uuid4())})
 
 def start_server():
     port = int(os.environ.get('PORT', 5000))
