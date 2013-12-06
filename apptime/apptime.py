d95af77686c389bc413a798c7496229169dfba1e
Danny Varner <danny.varner@gmail.com>
2013-12-05 18:40:59 -0800

    temporarily removing mongo connection

--- apptime/apptime.py
+++ apptime/apptime.py
@@ -11,9 +11,9 @@ logging.basicConfig(level=logging.INFO)
 
 @app.route("/apptime/devices/<id>/usage", methods=["GET", "POST"])
 
-client = MongoClient('mongodb://heroku_app20099764:94dacdf5097e43d72c784cdb50f0186a@ds053698.mongolab.com:53698/heroku_app20099764')
-db = client.get_default_database()
-collection = db.apptime_db
+#client = MongoClient('mongodb://heroku_app20099764:94dacdf5097e43d72c784cdb50f0186a@ds053698.mongolab.com:53698/heroku_app20099764')
+#db = client.get_default_database()
+#collection = db.apptime_db
 
 def hello(id):
     return flask.jsonify(**{ "usage": [
@@ -48,6 +48,6 @@ def devices():
 def start_server():
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
-	
+
 if __name__ == "__main__":
     start_server()
